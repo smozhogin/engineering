d5abe5fd386581111860cd83dff7c627572e036f
Sergey Mozhogin <s_mozhogin@yahoo.com>
2025-06-15 17:46:12 +0300

    Adding scripts

---
+++ dags/pipeline_dag.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from etl.data_loader import load_data
+from etl.data_preprocessor import preprocess_data
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'medic_pipeline',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False
+) as dag:    
+    
+    load_data = PythonOperator(
+        task_id='load_data',
+        python_callable=load_data
+    )
+    
+    preprocess_data = PythonOperator(
+        task_id='preprocess_data',
+        python_callable=preprocess_data
+    )
+    
+    load_data >> preprocess_data
